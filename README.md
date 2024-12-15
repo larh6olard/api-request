1aa6a4a95deaa6f537fb1cd5f65b793209c5e692
larh6olard <lasisihabeeb.lh@gmail.com>
2024-12-15 22:31:20 +0100

    Added some modification

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+# Check this out
 Built a fetch api request to get names, eye color and skin color of random star wars characters using javascript DOM.
+It is possible to get this error message "No 'Access-Control-Allow-Origin' header is present on the requested resource" while trying this out.
+## API change
+Changed the api from "https://swapi.dev/api/people/" to "https://swapi.py4e.com/api/people/"
--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function characterChange() {
   document.getElementById("skinColor").innerHTML = "Loading...";
 
   const randomNumber = Math.floor(Math.random() * 83)
-  
-  fetch(`https://swapi.dev/api/people/${randomNumber}/`)
-  .then( response => { return response.json() } )
-    .then( character => {
+
+  fetch(`https://swapi.py4e.com/api/people/${randomNumber}/`)
+    .then(response => { return response.json() })
+    .then(character => {
       const name = character['name'];
       const eyeColor = character['eye_color'];
       const skinColor = character['skin_color'];
@@ -18,6 +18,20 @@ function characterChange() {
       document.getElementById("name").innerHTML = name;
       document.getElementById("eyeColor").innerHTML = eyeColor;
       document.getElementById("skinColor").innerHTML = skinColor;
-    } )
-
+    });
 }
+
+// https://swapi.py4e.com/api/people/53/
+// fetch('https://swapi.dev/api/people/29/')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok ' + response.statusText);
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.error('There has been a problem with your fetch operation:', error);
+//   });
